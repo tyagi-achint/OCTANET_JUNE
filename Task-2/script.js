b5fc30a17a672ad055fd99e58a02767de23f1570
tyagi-achint <achinttyagi001@gmail.com>
2023-06-16 23:16:21 +0530

    day-1

---
+++ Task-2/script.js
@@ -0,0 +1,11 @@
+function clicked() {
+  var butt = document.getElementById("button");
+  butt.classList.add("buttonClick");
+  var element = document.getElementById("bike");
+  element.classList.add("listClick");
+}
+
+function abc() {
+  const element2 = document.getElementById("first");
+  element2.remove;
+}
